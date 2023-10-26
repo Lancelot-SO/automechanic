838782b7cfad56674c38b05cdbe684697d0f0192
zewdu erkyhun <zewdu444@gmail.com>
2023-10-26 12:21:20 +0300

    Add html tag for footers

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,7 +1,99 @@
 import React from "react";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <Footer>
+      {/* layer one */}
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <a href=''>
+                <span>About us</span>
+                <ul>
+                  <li>
+                    <a href=''>Contact</a>
+                  </li>
+                  <li>
+                    <a href=''>Our Partners</a>
+                  </li>
+                  <li>
+                    <a href=''>Advertising Online</a>
+                  </li>
+                  <li>
+                    <a href=''>Site Map</a>
+                  </li>
+                </ul>
+              </a>
+            </li>
+            <li>
+              <a href=''>
+                <span>Need Help?</span>
+              </a>
+              <ul>
+                <li>
+                  <a href=''>How do i add an offer?</a>
+                </li>
+                <li>
+                  <a href=''>How do I find a vehicle</a>
+                </li>
+                <li>
+                  <a href=''>Price list</a>
+                </li>
+                <li>
+                  <a href=''>Office for car dealers</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href=''>
+                <span>User area</span>
+              </a>
+              <ul>
+                <li>
+                  <a href=''>Add an offer</a>
+                </li>
+                <li>
+                  <a href=''>Register dealer</a>
+                </li>
+                <li>
+                  <a href=''>Login Dealer</a>
+                </li>
+                <li>
+                  <a href=''>News</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+        <div>
+          <h3>Find us Here</h3>
+          <ul>
+            <li>
+              <a href=''>Facebook</a>
+            </li>
+            <li>
+              <a href=''>Twitter</a>
+            </li>
+            <li>
+              <a href=''>Google</a>
+            </li>
+          </ul>
+          <p>Become our fan, and we will love you forever</p>
+        </div>
+      </div>
+      {/* layer two */}
+      <div>
+        <div>
+          <p>© 2012 Automarket. All Rights Reserved</p>
+          <div>
+            <p>Webdesign by</p>
+            <a href=''>Automarket</a>
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
 }
 
 export default Footer;
