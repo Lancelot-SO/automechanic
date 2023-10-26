958ea884e02d2cbbaa9c182f906c6f7de0016b6c
zewdu erkyhun <zewdu444@gmail.com>
2023-10-26 19:09:52 +0300

    Add carousal for home screen

---
+++ src/app/components/Carousel.js
@@ -0,0 +1,115 @@
+"use client";
+import React, { useRef } from "react";
+
+function Carousel() {
+  const sliderRef = useRef(null);
+  const totalSlides = 2; // Adjust this number based on the total number of slides.
+
+  const leftClickHandler = () => {
+    if (sliderRef.current) {
+      sliderRef.current.style.scrollBehavior = "smooth";
+      if (sliderRef.current.scrollLeft === 0) {
+        // If at the first slide, jump to the last slide
+        sliderRef.current.scrollLeft =
+          sliderRef.current.scrollWidth - sliderRef.current.offsetWidth;
+      } else {
+        sliderRef.current.scrollLeft -= sliderRef.current.offsetWidth;
+      }
+    }
+  };
+
+  const rightClickHandler = () => {
+    if (sliderRef.current) {
+      sliderRef.current.style.scrollBehavior = "smooth";
+      if (
+        sliderRef.current.scrollLeft >=
+        sliderRef.current.scrollWidth - sliderRef.current.offsetWidth
+      ) {
+        // If at the last slide, jump to the first slide
+        sliderRef.current.scrollLeft = 0;
+      } else {
+        sliderRef.current.scrollLeft += sliderRef.current.offsetWidth;
+      }
+    }
+  };
+  return (
+    <div className='carousel w-full rounded-md' style={{ overflowX: "scroll" }}>
+      <div
+        ref={sliderRef}
+        className='carousel-container'
+        style={{
+          display: "flex",
+          scrollSnapType: "x mandatory",
+          overflowX: "hidden",
+          scrollbarWidth: "none",
+          msOverflowStyle: "none",
+        }}
+      >
+        <div
+          id='slide1'
+          className='carousel-item relative w-full flex flex-col justify-center'
+          style={{ scrollSnapAlign: "start" }}
+        >
+          <img
+            src='/images/mercedes_full.jpg'
+            alt='Burger'
+            className='w-full rounded-md'
+          />
+          <div className='absolute flex justify-between transform -translate-y-1/2 left-1 right-1 top-1/2'>
+            <button
+              className='bg-white w-10 h-10 rounded-md hover:bg-[#F88220] transition duration-300 ease-in-out'
+              onClick={leftClickHandler}
+            >
+              ❮
+            </button>
+            <button
+              className='bg-white w-10 h-10 rounded-md hover:bg-[#F88220] transition duration-300 ease-in-out'
+              onClick={rightClickHandler}
+            >
+              ❯
+            </button>
+          </div>
+          <div className='relative mt-[-3rem]'>
+            <div className='flex flex-row justify-between bg-[#414141] text-2xl py-2 px-4'>
+              <p className='text-white'>Mercedes-Benz CLS 630</p>
+              <p className='text-[#F88220]'>54980 Euro</p>
+            </div>
+          </div>
+        </div>
+        <div
+          id='slide2'
+          className='carousel-item relative w-full flex flex-col justify-center'
+          style={{ scrollSnapAlign: "start" }}
+        >
+          <img
+            src='/images/mercedes_picture.jpg'
+            alt='Burger'
+            className='w-full rounded-md'
+          />
+          <div className='absolute flex justify-between transform -translate-y-1/2 left-1 right-1 top-1/2'>
+            <button
+              className='bg-white w-10 h-10 rounded-md hover:bg-[#F88220] transition duration-300 ease-in-out '
+              onClick={leftClickHandler}
+            >
+              ❮
+            </button>
+            <button
+              className='bg-white w-10 h-10 rounded-md hover:bg-[#F88220] transition duration-300 ease-in-out'
+              onClick={rightClickHandler}
+            >
+              ❯
+            </button>
+          </div>
+          <div className='relative mt-[-3rem]'>
+            <div className='flex flex-row justify-between bg-[#414141] text-2xl py-2 px-4'>
+              <p className='text-white'>Mercedes-Benz CLS 630</p>
+              <p className='text-[#F88220]'>54980 Euro</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Carousel;
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import Carousel from "./components/Carousel";
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between px-20 py-6 bg-[#F7F3F7]'>
@@ -11,7 +11,9 @@ export default function Home() {
               <strong>Best </strong>Offer
             </h3>
           </div>
-          <div>{/* image go here */}</div>
+          <div>
+            <Carousel />
+          </div>
         </div>
         <div>
           <div>
