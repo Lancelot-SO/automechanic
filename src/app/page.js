6c275608a6ebe9a6491604242710d9d587743729
zewdu erkyhun <zewdu444@gmail.com>
2023-10-26 14:19:05 +0300

    Add daisy UI element

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,32 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1 className='text-6xl font-bold text-center'>Home page</h1>
+    <main className='flex min-h-screen flex-col items-center justify-between px-20 py-6 bg-[#F7F3F7]'>
+      {/* section one */}
+      <section className='w-full flex flex-row'>
+        <div className='w-3/4'>
+          <div>
+            <h3 className='text-2xl'>
+              <strong>Best </strong>Offer
+            </h3>
+          </div>
+          <div>{/* image go here */}</div>
+        </div>
+        <div>
+          <div>
+            <strong>Latest</strong> offers
+          </div>
+          <ul>
+            <li>a</li>
+            <li>b</li>
+            <li>c</li>
+          </ul>
+        </div>
+      </section>
+      {/* section two */}
+      <section>section two</section>
+      {/* section three */}
+      <section>section three </section>
     </main>
   );
 }
