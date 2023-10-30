4eae190e2c1aaa1c9176ee825e042014e0a4fc77
Lancelot-SO <fsowah001@gmail.com>
2023-10-30 21:30:39 +0000

    created add offer page

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "accent-1": "#FAFAFA",
+        primary: "#7777777",
+        "accent-7": "#333",
+      },
     },
   },
   plugins: [require("daisyui")],
